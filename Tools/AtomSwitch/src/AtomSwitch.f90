61408f83b7cddc89bc0fbafa3f249fa128b6c124
nnardoto <nnardoto@gmail.com>
2025-03-28 15:32:15 -0300

    modified:   src/AtomSwitch.f90

--- Tools/AtomSwitch/src/AtomSwitch.f90
+++ Tools/AtomSwitch/src/AtomSwitch.f90
@@ -2,26 +2,33 @@ module AtomSwitch
   implicit none
   private
     integer nAtoms
-    real, allocatable :: R(:, :)
+    real(8), allocatable :: R(:, :)
+    real(8) X(3, 3)
+    real(8), parameter :: delta = 1E-3
+
+!    interface 
+!        function real f1(xx) 
+!    end interface
+
 
   public :: LoadSystem
+
 contains
   subroutine LoadSystem
     integer io, i, iov
-    real X(3, 3)
 
     integer     trash_int
     real        trash_real
     character   trash_char
     
-
-    open(action = 'read', newunit = io, FILE = 'input.inp')
     open(action = 'read', newunit =iov, FILE = 'vertice.inp')
-
         read(iov, *) X(1, :)
         read(iov, *) X(2, :)
         read(iov, *) X(3, :)
+    close(iov)
 
+
+    open(action = 'read', newunit = io, FILE = 'input.inp')
         read(io, *) nAtoms
         allocate(R(nAtoms, 3))
 
@@ -31,11 +38,35 @@ contains
     close(io)
 
         do i = 1, nAtoms
-            if (R(i, 1) >= X(1, 1) .and. R(i, 1) <= X(2, 1)) then
-                if(R(i, 2) >= X(1, 2) .and. R(i, 2) <= X(3, 2)) then
+!            if (R(i, 1) >= X(1, 1) .and. R(i, 1) <= X(2, 1)) then
+!                if(R(i, 2) >= X(1, 2) .and. R(i, 2) <= X(3, 2)) then
+                if(R(i, 2) >= X(1, 2) .and. R(i, 2) <= f1(R(i, 1)) .and. R(i, 2) <= f2(R(i, 1) )) then
                     write(*, *) i, R(i, :)
                 endif
-            endif
+!            endif
         enddo
   end subroutine LoadSystem
+
+  real(8) function f1(xx)
+    real(8) :: m, b
+    real(8), intent(IN)  :: xx
+
+    m = (X(3, 2) - X(1, 2)) / (X(3, 1) - X(1, 1))
+    b = X(1, 2) - m*X(1, 1)
+    
+    f1 = m*xx + b + delta
+    return
+  end function
+
+  real(8) function f2(xx)
+    real(8) :: m, b
+    real(8), intent(IN)  :: xx
+  
+    m = (X(3, 2) - X(2, 2)) / (X(3, 1) - X(2, 1))
+    b = X(2, 2) - m*X(2, 1)
+    
+    f2 = m*xx + b + delta
+    return
+  end function
+  
 end module AtomSwitch
